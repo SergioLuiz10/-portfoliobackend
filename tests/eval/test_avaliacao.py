270bbc874ec109bb1aee1d7c28d163f3f49e9983
SergioLuiz10 <sergioluizteixeira12345@gmail.com>
2026-07-06 20:56:29 -0300

    feat automated avaliacao test

--- tests/eval/test_avaliacao.py
+++ tests/eval/test_avaliacao.py
@@ -4,6 +4,9 @@ from tests.eval.dataset import CASOS_AVALIACAO#chama os casos fakes pra comparar
 
 #pra cada caso o retrieval trouxe o chunk certo?
 def test_retriever():
+    #quantos casos acertaram. Começa em zero
+    acertos = 0
+
     #passa de uma vez por cada um dos 7
     #A cada volta, a variável caso vira um CasoAvaliacao
     for caso in CASOS_AVALIACAO:
@@ -13,16 +16,23 @@ def test_retriever():
         textos_chunks_parecidos=[chunk.page_content for chunk in chunks_parecidos]
        
        
-        #junta os 3 textos num textao pra facilitar a busca da palabra
+        #junta os 3 textos num textao 
         textaofull_chunks=" ".join(textos_chunks_parecidos)
-        # Deixa o textão todo em minúsculo pra comparação não depender de maiúscula.
+        # Deixa o textão todo em minúsculo 
         texto_completo_minusculo = textaofull_chunks.lower()
 
-        # Pega a 1ª palavra-chave do caso, também em minúsculo.
+        # Pega a 1ª palavra-chave do caso
         palavra_chave = caso.palavras_chave[0].lower()
 
         # O caso "acerta" se a palavra-chave aparece em algum lugar do textão.
         acertou = palavra_chave in texto_completo_minusculo
+        #se acertou, soma 1 no total de acertos
+        if acertou:
+            acertos += 1
+    # calcula a porcentagem de acertos
+    total_de_casos = len(CASOS_AVALIACAO)   
 
-
-
+    # Divide os acertos pelo total pra virar uma nota de 0 a 1
+    score = acertos / total_de_casos 
+    # printa a nota de acertos em porcentagem
+    print(f"\nContext relevance: {acertos}/{total_de_casos} = {score:.2%}")
