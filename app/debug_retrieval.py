94dae8d5f920e35edcc8c7d9ce4b5cd2cc90c00d
SergioLuiz10 <sergioluizteixeira12345@gmail.com>
2026-07-09 14:43:06 -0300

    feat retriever script

---
+++ app/debug_retrieval.py
@@ -0,0 +1,18 @@
+from app.rag.retriever import buscar_chunks_parecidos
+
+
+#pergunta q falhou no teste de avaliação
+#jogar ela no banco pra ver quais chunks voltaram
+pergunta = "Onde o Sérgio estuda e quando se forma?"
+
+
+#pega os chunks parecidos com a pergunta q falhou no teste de avaliação
+chunks = buscar_chunks_parecidos(pergunta)
+
+
+# printa os chunks que vieram do banco, pra ver se tem algum q é parecido com a resposta certa  
+for i, chunk in enumerate(chunks):
+    print(f"\n--- CHUNK {i} ---")
+    #mostra o texto do chunk q veio do banco
+    print(chunk.page_content) 
+
